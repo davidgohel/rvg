2c035c92be1cd65a8d2b3aac4d0930ea275ddcc8
David Gohel <david.gohel@ardata.fr>
2015-12-31 15:47:43 +0100

    add tests

--- tests/testthat/test-interactive-svg.R
+++ tests/testthat/test-interactive-svg.R
@@ -18,3 +18,61 @@ file <- tempfile(fileext = ".svg")
   expect_equal(circle_id, as.character(1:2) )
 })
 
+test_that("tooltips are written", {
+  file <- tempfile(fileext = ".svg")
+  dsvg( file = file, standalone = FALSE, canvas_id = 1 )
+  plot.new()
+  rvg_tracer_on()
+  points(c(0.5, .6), c(.4, .3))
+  ids = rvg_tracer_off()
+  send_tooltip(ids = ids, tooltips = c("tip1", "tip2") )
+  dev.off()
+
+  doc <- read_xml(file)
+  script_node <- xml_find_one(doc, ".//script")
+  script_txt <-  xml_text(script_node)
+  tip1 <- "attr\\(\\'title\\',\\'tip1\\'\\)"
+  tip2 <- "attr\\(\\'title\\',\\'tip2\\'\\)"
+  expect_true( grepl( tip1, script_txt ) )
+  expect_true( grepl( tip2, script_txt ) )
+})
+
+test_that("ID are written", {
+  file <- tempfile(fileext = ".svg")
+  dsvg( file = file, standalone = FALSE, canvas_id = 1 )
+  plot.new()
+  rvg_tracer_on()
+  points(c(0.5, .6), c(.4, .3))
+  ids = rvg_tracer_off()
+  set_data_id(ids = ids, data_id = c("HI1", "HI2") )
+  dev.off()
+
+  doc <- read_xml(file)
+  script_node <- xml_find_one(doc, ".//script")
+  script_txt <-  xml_text(script_node)
+  tip1 <- "attr\\(\\'data-id\\',\\'HI1\\'\\)"
+  tip2 <- "attr\\(\\'data-id\\',\\'HI2\\'\\)"
+  expect_true( grepl( tip1, script_txt ) )
+  expect_true( grepl( tip2, script_txt ) )
+})
+
+
+test_that("clicks are written", {
+  file <- tempfile(fileext = ".svg")
+  dsvg( file = file, standalone = FALSE, canvas_id = 1 )
+  plot.new()
+  rvg_tracer_on()
+  points(c(0.5, .6), c(.4, .3))
+  ids = rvg_tracer_off()
+  send_click(ids = ids, clicks = c("alert(1)", "alert(2)") )
+  dev.off()
+
+  doc <- read_xml(file)
+  script_node <- xml_find_one(doc, ".//script")
+  script_txt <-  xml_text(script_node)
+  tip1 <- "click\\(alert\\(1\\)\\)"
+  tip2 <- "click\\(alert\\(2\\)\\)"
+  expect_true( grepl( tip1, script_txt ) )
+  expect_true( grepl( tip2, script_txt ) )
+})
+
