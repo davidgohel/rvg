6d553bd98414e99a84327cac603db2956875c6f7
David Gohel <david.gohel@ardata.fr>
2015-12-31 12:36:58 +0100

    weird fix
    order matters in a:pPr tags!

--- src/ppr.cpp
+++ src/ppr.cpp
@@ -17,8 +17,8 @@ std::string ppr::a_tag()
     os << " algn=\"r\"";
   os << " marL=\"0\" marR=\"0\" indent=\"0\">";
   os << "<a:lnSpc><a:spcPts val=\"" << (int)(this->lineheight*100) << "\"/></a:lnSpc>";
-  os << "<a:spcAft><a:spcPts val=\"0\"/></a:spcAft>";
   os << "<a:spcBef><a:spcPts val=\"0\"/></a:spcBef>";
+  os << "<a:spcAft><a:spcPts val=\"0\"/></a:spcAft>";
   os << "</a:pPr>";
   return os.str();
 }
