110a3e41de8dca2adc16b4515a99138ca853fe19
David Gohel <david.gohel@ardata.fr>
2016-01-03 10:57:14 +0100

    add dml text tests

--- tests/testthat/test-docx-xfrm.R
+++ tests/testthat/test-docx-xfrm.R
@@ -1,7 +1,7 @@
 context("DOCX shape dimensions")
 library(xml2)
 
-test_that("rect dimensions", {
+test_that("rect has dimensions", {
 
   file <- tempfile()
 
@@ -28,7 +28,7 @@ test_that("rect dimensions", {
   expect_true( grepl("^[0-9]+$", cy ) )
 })
 
-test_that("lines dimensions", {
+test_that("lines has dimensions", {
 
   file <- tempfile()
 
@@ -55,7 +55,7 @@ test_that("lines dimensions", {
   expect_true( grepl("^[0-9]+$", cy ) )
 })
 
-test_that("polygon dimensions", {
+test_that("polygon has dimensions", {
 
   file <- tempfile()
 
@@ -68,6 +68,32 @@ test_that("polygon dimensions", {
   polygon(x, y, lwd = 1)
   dev.off()
 
+
+  doc <- read_xml(file)
+  xfrm_node <- xml_find_one(doc, ".//wps:wsp/wps:spPr/a:xfrm", ns = xml_ns( doc ))
+  expect_is(object = xfrm_node, class = "xml_node")
+
+  off_node <- xml_find_one(doc, ".//wps:wsp/wps:spPr/a:xfrm/a:off", ns = xml_ns( doc ))
+  offx <- xml_attr(off_node, "x")
+  offy <- xml_attr(off_node, "y")
+  expect_true( grepl("^[0-9]+$", offx ) )
+  expect_true( grepl("^[0-9]+$", offy ) )
+
+  ext_node <- xml_find_one(doc, ".//wps:wsp/wps:spPr/a:xfrm/a:ext", ns = xml_ns( doc ))
+  cx <- xml_attr(ext_node, "cx")
+  cy <- xml_attr(ext_node, "cy")
+  expect_true( grepl("^[0-9]+$", cx ) )
+  expect_true( grepl("^[0-9]+$", cy ) )
+})
+
+test_that("text has dimensions", {
+
+  file <- tempfile()
+
+  dml_docx( file = file, bg = "transparent" )
+  plot.new()
+  text(x = .5, y = .5, labels = "test")
+  dev.off()
 
   doc <- read_xml(file)
   xfrm_node <- xml_find_one(doc, ".//wps:wsp/wps:spPr/a:xfrm", ns = xml_ns( doc ))
--- tests/testthat/test-pptx-xfrm.R
+++ tests/testthat/test-pptx-xfrm.R
@@ -1,7 +1,7 @@
 context("PPTX shape dimensions")
 library(xml2)
 
-test_that("rect dimensions", {
+test_that("rect has dimensions", {
 
   file <- tempfile()
 
@@ -28,7 +28,7 @@ test_that("rect dimensions", {
   expect_true( grepl("^[0-9]+$", cy ) )
 })
 
-test_that("lines dimensions", {
+test_that("lines has dimensions", {
 
   file <- tempfile()
 
@@ -55,7 +55,7 @@ test_that("lines dimensions", {
   expect_true( grepl("^[0-9]+$", cy ) )
 })
 
-test_that("polygon dimensions", {
+test_that("polygon has dimensions", {
 
   file <- tempfile()
 
@@ -66,6 +66,34 @@ test_that("polygon dimensions", {
   x <- c(x[1], x[1], x[2], x[2], NA )
   y <- c(y[2], y[1], y[1], y[2], NA )
   polygon(x, y, lwd = 1)
+  dev.off()
+
+
+  doc <- read_xml(file)
+  xfrm_node <- xml_find_one(doc, ".//p:sp/p:spPr/a:xfrm", ns = xml_ns( doc ))
+  expect_is(object = xfrm_node, class = "xml_node")
+
+  off_node <- xml_find_one(doc, ".//p:sp/p:spPr/a:xfrm/a:off", ns = xml_ns( doc ))
+  offx <- xml_attr(off_node, "x")
+  offy <- xml_attr(off_node, "y")
+  expect_true( grepl("^[0-9]+$", offx ) )
+  expect_true( grepl("^[0-9]+$", offy ) )
+
+  ext_node <- xml_find_one(doc, ".//p:sp/p:spPr/a:xfrm/a:ext", ns = xml_ns( doc ))
+  cx <- xml_attr(ext_node, "cx")
+  cy <- xml_attr(ext_node, "cy")
+  expect_true( grepl("^[0-9]+$", cx ) )
+  expect_true( grepl("^[0-9]+$", cy ) )
+})
+
+
+test_that("text has dimensions", {
+
+  file <- tempfile()
+
+  dml_pptx( file = file, bg = "transparent" )
+  plot.new()
+  text(x = .5, y = .5, labels = "test")
   dev.off()
 
 
