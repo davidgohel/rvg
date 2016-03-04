06f7c9fe8672b97845393bc7ea884b6bf019c49e
David Gohel <david.gohel@ardata.fr>
2016-03-04 10:44:31 +0100

    test editable prop

---
+++ tests/testthat/test-dml-editable.R
@@ -0,0 +1,48 @@
+context("Test DrawingML editable properties")
+library(xml2)
+
+test_that("check docx editable properties", {
+
+  file <- tempfile()
+  dml_docx( file = file, editable = FALSE, bg = "transparent" )
+  plot.new()
+  points(0.2, 0.2)
+  dev.off()
+
+  x <- read_xml(file)
+  xpath_ <- ".//wps:wsp/wps:cNvSpPr/a:spLocks"
+  node <- try( xml_find_one(x, xpath_, ns = xml_ns( x )), silent = TRUE )
+  expect_is(object = node, class = "xml_node")
+
+  file <- tempfile()
+  dml_docx( file = file, editable = TRUE, bg = "transparent" )
+  plot.new()
+  points(0.2, 0.2)
+  dev.off()
+  x <- read_xml(file)
+  xpath_ <- ".//wps:wsp/wps:cNvSpPr/a:spLocks"
+  expect_error(object = xml_find_one(x, xpath_, ns = xml_ns( x )), regexp = "No matches")
+})
+
+test_that("check pptx editable properties", {
+
+  file <- tempfile()
+  dml_pptx( file = file, editable = FALSE, bg = "transparent" )
+  plot.new()
+  points(0.2, 0.2)
+  dev.off()
+
+  x <- read_xml(file)
+  xpath_ <- ".//p:sp/p:nvSpPr/p:cNvSpPr/a:spLocks"
+  node <- try( xml_find_one(x, xpath_, ns = xml_ns( x )), silent = TRUE )
+  expect_is(object = node, class = "xml_node")
+
+  file <- tempfile()
+  dml_pptx( file = file, editable = TRUE, bg = "transparent" )
+  plot.new()
+  points(0.2, 0.2)
+  dev.off()
+  x <- read_xml(file)
+  xpath_ <- ".//p:sp/p:nvSpPr/p:cNvSpPr/a:spLocks"
+  expect_error(object = xml_find_one(x, xpath_, ns = xml_ns( x )), regexp = "No matches")
+})
