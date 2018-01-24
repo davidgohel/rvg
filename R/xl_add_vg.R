e505a234d3ca6dd6cfd54e7e0b7476efa1c49716
dgohel <david.gohel@ardata.fr>
2018-01-24 23:19:08 +0100

    man - minor

--- R/xl_add_vg.R
+++ R/xl_add_vg.R
@@ -15,7 +15,7 @@
 #' library(officer)
 #' my_ws <- read_xlsx()
 #' my_ws <- xl_add_vg(my_ws, sheet = "Feuil1",
-#'   code = print(gg), width = 6, height = 6, left = 1, top = 2 )
+#'   code = barplot(1:5, col = 2:6), width = 6, height = 6, left = 1, top = 2 )
 #' print(my_ws, target = "vg.xlsx")
 #' }
 xl_add_vg <- function( x, sheet, code, left, top, width, height, ... ){
