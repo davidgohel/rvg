e50c71ca15c346219d61a6fcc46a3a7894342962
David Gohel <david.gohel@ardata.fr>
2022-12-08 09:39:04 +0100

    fix: doc ... in body_add_vg

--- R/defunct.R
+++ R/defunct.R
@@ -4,6 +4,7 @@ NULL
 
 #' @export
 #' @rdname rvg-defunct
+#' @param ... unused argument
 #' @details `body_add_vg()` is removed and can be replaced by `officer::body_add_img`.
 body_add_vg <- function(...){
   .Defunct("officer::body_add_img")
